37f5c65204e4af46f70ffd6cee8d4382d37056f2
Derrick Adkins <derrick.l.adkins@gmail.com>
2021-02-28 18:24:19 -0500

    image animations
    add image fade-ins on scroll and reworked js to be cleaner and more efficient

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+//Cache reference to window and animation items
+var $animation_elements;
+var $window = $(window);
 var menu_show = true;
 var $logo;
 
@@ -21,67 +24,109 @@ $(document).ready(function(){
 		}
 	});
 	
+	$animation_elements = $(".img_wrapper");
 	$logo = $("#header_logo_wrapper");
-	
 	$logo.data('size','big');
-	$(window).on("scroll resize", function(){
+	
+	//calc nav width
+	var nw = 0;
+	$(".top_lvl").each(function(i, a){
+		//console.log($(this).outerWidth(true));
+		nw += $(this).outerWidth(true);
+	});
+	nw += 30;//why?
+	
+	//console.log("top_lvl width = " + nw);
+	
+	//chache vars
+	var ww, st, lw, lh, nw, nh, ew, hh;
+	var $externals = $("header .externals");
+	var $nav = $("#nav_wrapper");
+	var $logoImg = $("#header_logo");
+	var $nav2 = $(".nav_2");
+	$window.on("scroll resize", function(){
+		ww = $window.width();
+		st = $(document).scrollTop();
+		
+		//console.log("resize to " + $window.width());
+		//console.log("scroll to " + st);
+		
+		//fade-ins
+		//check_if_in_view();
+		
 		//clac logo size
-		var w, h;
-		if($(window).width() < 272){
-			w = 0; //display:none
+		if(ww < 272){
+			lw = 0; //display:none
 		}
-		else if($(window).width() < 500){
-			w = $(window).scrollTop() == 0 ? $(window).width()-50 : 222;
+		else if(ww < 500){
+			lw = st == 0 ? ww-50 : 222;
 		}else{
-			w = $(window).scrollTop() == 0 ? 400 : 222;
+			lw = st == 0 ? 400 : 222;
 		}
-		h = w*676/3000;
+		lh = lw*676/3000 + 4;//wrapping logo anchor tag add 4 pixels
+		
+		$logo.show();
+		if(ww < lw+100) $logo.css("margin-left",50);
+		else $logo.css("margin","auto");		
+		
+		ew = $externals.width();
+		
+		//dynamic css
+		nh = 0;
+		if(ww > lw+nw+ew){
+			$externals.show();
+			$nav.css({"flex":"1", "order":"0", "display":"block"});
+			$logoImg.css("float", "none");
+			$nav2.hide();
+		}else if(ww > nw){
+			$externals.show();
+			$nav.css({"flex":"100%", "order":"1", "display":"block"});
+			$logoImg.css("float", "left");
+			$nav2.hide();
+			lh -= 4; //except in this case ???
+			nh = $(".top_lvl").height();
+		}
+		else{
+			$externals.hide();
+			$nav.hide();
+			$logoImg.css("float", "none");
+			$nav2.show();
+			$("#menu_close").hide();
+		}
 		
 		//calc header height
-		var nav_height = 32;
-		var hh = $(window).width() < w + $("#nav_1").width() ? h + nav_height : h;
-		hh += 4; //wrapping anchor tag add 4 pixels
-		if(w == 0){
+		var hh = lh + nh;
+		if(lw == 0){
 			$logo.hide();
 			return;
 		}
 		
-		$logo.show();
-		if($(window).width() < w+100) $logo.css("margin-left",50);
-		else $logo.css("margin","auto");
+		/*
+		console.log("ww = " + ww + " hw = " + (lw+nw+ew) + 
+					"\nlw = " + lw + " nw = " + nw + " ew = " + ew + 
+					"\nlh = " + lh + " hh = " + hh);
+		*/
 		
 		//animate if needed
-		if($(document).scrollTop() > 0 && $logo.data('size') == 'big'){
+		if(st > 0 && $logo.data('size') == 'big'){
 			$logo.data('size','small');
-			$logo.stop().animate({width:w},300,padd_main(true,hh));
-		}else if($(document).scrollTop() == 0 && $logo.data('size') == 'small'){
+			$logo.stop().animate({width:lw},300,padd_main(true,hh));
+		}else if(st == 0 && $logo.data('size') == 'small'){
 			$logo.data('size','big');
-			$logo.stop().animate({width:w},300,padd_main(true,hh));
+			$logo.stop().animate({width:lw},300,padd_main(true,hh));
 		}else{
-			$logo.width(w);
-			padd_main();
+			$logo.width(lw);
+			padd_main(false, hh);
 		}
-		
-		//dynamic css
-		if(($(document).scrollTop() == 0 && $(window).width() < 1530) 
-			|| ($(document).scrollTop() > 0 && $(window).width() < 1336))
-			$("header .externals").hide();
-		else $("header .externals").show();
-		
-		if(($(document).scrollTop() == 0 && $(window).width() < 1212) 
-			|| ($(document).scrollTop() > 0 && $(window).width() < 1035))
-			$("#nav_wrapper").css("flex", "100%");
-		else $("#nav_wrapper").css("flex", "1");
 	});
 	
-	//$(window).resize(function(){console.log("resize to " + $(window).width());});
-	//$(window).scroll(function(){console.log("scroll to " + $(document).scrollTop());});
-	
-	$(window).trigger("scroll");
+	$(document).trigger("scroll");
 });
 
 function padd_main(animate = false, height = $("header").height()){
+	//console.log("padd_main " + height + " animate = " + animate);
 	if(animate){
+		var t = $(document).scrollTop();
 		$("main").animate({
 			"padding-top":height
 		},300);
@@ -103,4 +148,48 @@ function menu_toggle(){
 		$("#menu_open").show();
 	}
 	menu_show = !menu_show;
+}
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+  
+  //console.log("wh " + window_height);
+  //console.log("wt " + window_top_position);
+  //console.log("wb " + window_bottom_position);
+  
+  //console.log("ael " + $animation_elements.length);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.height();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+	
+	//console.log("eh " + element_height);
+	//console.log("et " + element_top_position);
+	//console.log("eb " + element_bottom_position);
+
+    //check to see if this current container is within viewport
+    if (element_bottom_position >= window_top_position &&
+        element_top_position <= window_bottom_position) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+	
+	/*
+	var x = 60;
+    if (!$element.hasClass("in-view") && 
+		(element_bottom_position-x >= window_top_position) &&
+        (element_top_position+x <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else if($element.hasClass("in-view") &&
+		((element_bottom_position-(x-10) <= window_top_position) ||
+        (element_top_position+(x-10) >= window_bottom_position))) {
+      $element.removeClass('in-view');
+    }
+	*/
+  });
 }
